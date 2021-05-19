4e4e86ec09c0f0651430cf6c4402d6d584657f75
Laís Galvão <laisgbueno62@gmail.com>
2021-05-18 23:28:01 -0300

    Add header + novo bg

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,11 +2,16 @@
   <div>
     <Header />
     <div class="desce-jumbo">
-      <b-jumbotron header="Elétrica Residencial Galvão" lead="Eletricista Residencial e Projetos de Elétrica">
+      <b-jumbotron text-variant="light" header="Elétrica Residencial Galvão" lead="Eletricista Residencial e Projetos de Elétrica">
         <p>Mais informações</p>
         <b-button variant="danger" href="#">More Info</b-button>
       </b-jumbotron>
+     
     </div>
+     <div class="container-fluid">
+        
+      </div>
+      <div class="parallax"></div>
   </div>
 </template>
 
@@ -26,11 +31,28 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .jumbotron{
-  background: url('../assets/bgparallax.jpg') no-repeat fixed center;
+  background: url('../assets/banner.png') no-repeat fixed center;
   background-size: cover;
-  height: 80vh;
+  height: 95vh;
 }
 .desce-jumbo{
-  margin-top: 25px;
+  margin-top: 45px;
 }
+.jumbotron-header{
+   filter: opacity(0.9);
+  background-color: #000;
+}
+
+.container-fluid{
+  height: 30vh;
+  position: relative;
+}
+
+.parallax{
+  position: absolute;
+   background: url('../assets/banner.png') no-repeat fixed center;
+    background-size: cover;
+    height: 95vh;
+}
+
 </style>
--- src/components/Interface/Header.vue
+++ src/components/Interface/Header.vue
@@ -4,17 +4,9 @@
     <b-navbar-nav>
       <b-nav-item href="#">Home</b-nav-item>
 
-      <!-- Navbar dropdowns -->
-      <b-nav-item-dropdown text="Lang" right>
-        <b-dropdown-item href="#">EN</b-dropdown-item>
-        <b-dropdown-item href="#">ES</b-dropdown-item>
-        <b-dropdown-item href="#">RU</b-dropdown-item>
-        <b-dropdown-item href="#">FA</b-dropdown-item>
-      </b-nav-item-dropdown>
-
-      <b-nav-item-dropdown text="User" right>
-        <b-dropdown-item href="#">Account</b-dropdown-item>
-        <b-dropdown-item href="#">Settings</b-dropdown-item>
+      <b-nav-item-dropdown text="Mais">
+        <b-dropdown-item href="#">Contato</b-dropdown-item>
+        <b-dropdown-item href="#">Fale Comigo</b-dropdown-item>
       </b-nav-item-dropdown>
     </b-navbar-nav>
   </b-navbar>
@@ -32,6 +24,6 @@ export default {
     width: 100%;
     height: 60px;
     position: fixed;
-    margin-top: -25px;
+    margin-top: -50px;
 }
 </style>
