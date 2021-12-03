9bd5cb7724b2640c7c3a00227ea43923bbe0e9a1
Laís <laisgbueno62@gmail.com>
2021-12-02 22:16:16 -0300

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+git push -f https://github.com/LaisGalvao/Marcio_Eletrica.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+#git push -f git@github.com:https://github.com/LaisGalvao/LaisGalvao.github.io.git master:master
+#git push -f git@github.com:LaisGalvao/LaisGalvao.github.io.git master:master
+
+cd -
