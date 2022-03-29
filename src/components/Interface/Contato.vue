ce9a55b12983c95ceb53414261314ef07b4aa593
Laís Galvão <laisgbueno62@gmail.com>
2022-03-29 20:33:20 -0300

    form de contato envando email + mensagem de log

--- src/components/Interface/Contato.vue
+++ src/components/Interface/Contato.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="text-center">
-    <form class="form-contato" ref="form" @submit="sendMail">
+    <!--   <form class="form-contato" ref="form" @submit="sendMail">
       <b-row>
         <b-col cols="6"
           ><span>Nome:</span>
@@ -73,7 +73,31 @@
           >
         </b-col>
       </b-row>
+    </form> -->
+
+    <form @submit.prevent="sendMail">
+      <label>Name</label>
+      <input type="text" v-model="name" name="name" placeholder="Your Name" />
+      <label>Email</label>
+      <input
+        type="email"
+        v-model="email"
+        name="email"
+        placeholder="Your Email"
+      />
+      <label>Message</label>
+      <textarea
+        name="message"
+        v-model="message"
+        cols="30"
+        rows="5"
+        placeholder="Message"
+      >
+      </textarea>
+
+      <input type="submit" value="Send" />
     </form>
+
     <b-button
       variant="outline-success"
       class="btn-modal-contato"
@@ -85,13 +109,11 @@
 </template>
 <script>
 import emailjs from "emailjs-com";
-/* import axios from "axios"; */
-/* const querystring = require("querystring"); */
 
 export default {
   data() {
     return {
-      form: {
+      /*   form: {
         name: "",
         email: "",
         tel: "",
@@ -105,45 +127,37 @@ export default {
           { value: "padrao_entrada", text: "Padrão de Entrada" },
           { value: "projeto_eletrico", text: "Projetos Elétricos" }
         ]
-      },
-      obj: null
+      }, */
+      obj: null,
+      name: "",
+      email: "",
+      message: ""
     };
   },
   methods: {
     sendMail(e) {
-      /* var newForm = Object.assign(this.form, this.selected, {}); */
-      /*   console.log(newForm); */
-      /*    axios
-        .post("../../services/mail.php", querystring.stringify(this.form))
-        .then((res) => console.log(res.data, e)); */
       try {
-        emailjs
-          .sendForm(
-            "service_galvaoeletrica",
-            "template_2dhvsup",
+        emailjs.sendForm(
+          "service_galvaoeletrica",
+          "template_2dhvsup",
+          e.target,
+          "user_1zmTo7op4jjuKsXYkSaHV",
 
-            "user_1zmTo7op4jjuKsXYkSaHV",
-            e.target,
-            {
-              name: this.form.name,
-              email: this.form.email,
-              message: this.form.msg
-            }
-          )
-          .then(
-            result => {
-              console.log("SUCCESS!", result.text, result.status, result.data);
-            },
-            error => {
-              console.log("FAILED...", error.text);
-            }
-          );
+          {
+            name: this.name,
+            email: this.email,
+            message: this.msg
+          },
+          alert("deu certo")
+        );
       } catch (error) {
+        alert("bugou");
         console.log(error);
       }
 
-      localStorage.setItem("form", this.form);
-      e;
+      this.name = "";
+      this.email = "";
+      this.msg = "";
     },
     send() {
       /*  this.obj = {
@@ -179,5 +193,44 @@ label {
 }
 span {
   text-align: center;
+}
+
+* {
+  box-sizing: border-box;
+}
+label {
+  float: left;
+}
+input[type="text"],
+[type="email"],
+textarea {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  margin-top: 6px;
+  margin-bottom: 16px;
+  resize: vertical;
+}
+input[type="submit"] {
+  background-color: #4caf50;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+input[type="submit"]:hover {
+  background-color: #b8c7b9;
+}
+.container {
+  display: block;
+  margin: auto;
+  text-align: center;
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+  width: 50%;
 }
 </style>
