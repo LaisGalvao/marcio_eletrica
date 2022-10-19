5c89c121e5402ea990d4af3c8537d352294d74f5
Laís Galvão <laisgbueno62@gmail.com>
2022-10-19 16:32:00 -0300

    update api url

--- src/services/index.js
+++ src/services/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   // baseURL: 'http://127.0.0.1:8000/api',
-  baseURL: "https://marcio-eletrica-backend.herokuapp.com/api",
+  baseURL: "https://marcio-eletrica-backend-production.up.railway.app//api",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json"
